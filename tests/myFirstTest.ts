a0e09884d6310b178b115076c03d9e984cf80ac5
christian.ciceri <christian.ciceri@apiumtech.com>
2016-08-16 18:23:42 +0200

    added typings

--- tests/myFirstTest.ts
+++ tests/myFirstTest.ts
@@ -1,10 +1,11 @@
 import {Hero} from '../app/hero';
 import {Subject} from 'rxjs/Rx';
+import {expect} from 'chai';
 
 describe("first test", () => {
     it('has name', () => {
         let hero:Hero = {id: 2, name: 'Super Cat'}; //structural type
-        expect(hero.name).toEqual('Super Cat');
+        expect(hero.name).to.equal('Super Cat');
         let a = new Subject();
         a.subscribe(()=>console.log("hola 2"));
         a.next({})
