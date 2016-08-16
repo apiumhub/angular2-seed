8fff5fa98f9b84ba6224f05e01e3e6e2d5908d8b
christian.ciceri <christian.ciceri@apiumtech.com>
2016-08-16 18:25:48 +0200

    added a test

--- tests/myFirstTest.ts
+++ tests/myFirstTest.ts
@@ -1,11 +1,15 @@
 import {Hero} from '../app/hero';
 import {Subject} from 'rxjs/Rx';
+//noinspection TypeScriptCheckImport
 import {expect} from 'chai';
 
 describe("first test", () => {
     it('has name', () => {
         let hero:Hero = {id: 2, name: 'Super Cat'}; //structural type
         expect(hero.name).to.equal('Super Cat');
+    });
+    it("can subscribe", function()
+    {
         let a = new Subject();
         a.subscribe(()=>console.log("hola 2"));
         a.next({})
