8c5583c3c4ae3ed02ce8ed1e7221886653d25653
christian.ciceri <christian.ciceri@apiumtech.com>
2016-09-08 15:54:57 +0200

    greenbar (with correct redbar)

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -10,6 +10,7 @@ import {Observer} from "rxjs/Observer";
 import {AjaxObservable} from "rxjs/observable/dom/AjaxObservable";
 import axios from 'axios';
 import {Subscription} from "rxjs/Subscription";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
 
 export const HEROES:Hero[] = [
     {id: 11, name: 'Mr. Nice'},
@@ -48,18 +49,21 @@ export class HeroService {
     }
 
     loadHeroes():Subscription {
-        return Observable.defer(() => axios.create({
-                baseURL: 'http://localhost:3004/',
-                timeout: 4000,
-                headers: {'X-Custom-Header': 'foobar'}
-            }).get('/heroes'))
-            .retry(3)
-            .subscribe(
-                (resp:string) => this.heroesRefreshed.next(HEROES),
-                (error) => (
-                    console.log(JSON.stringify(error.message), error.code)
-                )
-            );
-
+        var responseStream = new BehaviorSubject('/')
+                .flatMap(requestUrl => Observable.fromPromise(axios.create({
+                                baseURL: 'http://localhost:3004/',
+                                timeout: 1000,
+                                headers: {'X-Custom-Header': 'foobar'}
+                            }).get(requestUrl)));
+        responseStream.next('/heroes');
+        return responseStream
+                .retry(3)
+                .do((resp) => {console.log(JSON.stringify(resp));})
+                .map((resp) => resp.data )
+                .subscribe(
+                  (heroes: Hero[]) => {return this.heroesRefreshed.next(heroes)},
+                  (err:Error) => console.log('Error: ' + err),
+                  () => console.log('Completed'));
     }
 }
+//array map extension
