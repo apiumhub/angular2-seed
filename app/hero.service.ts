6689badba29ee9f0158fd1409865697b0d6e1f93
christian.ciceri <christian.ciceri@apiumtech.com>
2016-09-08 16:55:47 +0200

    extracted gateways

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -11,28 +11,9 @@ import {AjaxObservable} from "rxjs/observable/dom/AjaxObservable";
 import axios from 'axios';
 import {Subscription} from "rxjs/Subscription";
 import {BehaviorSubject} from "rxjs/BehaviorSubject";
+import {Gateway, AxiosGateway} from './utils/gateways'
 
-interface Gateway<T> {
-    get(resource:string):Observable<T>;
-}
-class AxiosGateway<T> implements Gateway<T> {
-    get(resource:string):Observable<T> {
-        const subject = new BehaviorSubject<string>('/');
-        subject.next(resource);
-        return subject
-            .flatMap(resource => Observable.fromPromise(axios.request({
-                baseURL: 'http://localhost:3004/',
-                timeout: 1000,
-                method: 'get',
-                headers: {'X-Custom-Header': 'foobar'},
-                url: resource
-            }))).retry(3)
-            .do((resp) => {
-                console.log("returned from call for resource: ", resource, JSON.stringify(resp));
-            })
-            .map((resp) => resp.data);
-    }
-}
+
 
 export const HEROES:Hero[] = [
     {id: 11, name: 'Mr. Nice'},
---
+++ app/utils/gateways.ts
@@ -0,0 +1,35 @@
+/**
+ * Created by christian on 8/09/16.
+ */
+import {Observable, Subject} from 'rxjs/Rx';
+import 'rxjs/add/observable/dom/ajax';
+import 'rxjs/add/observable/fromPromise';
+import 'rxjs/add/observable/defer';
+import 'rxjs/add/operator/retry';
+import {Observer} from "rxjs/Observer";
+import {AjaxObservable} from "rxjs/observable/dom/AjaxObservable";
+import axios from 'axios';
+import {Subscription} from "rxjs/Subscription";
+import {BehaviorSubject} from "rxjs/BehaviorSubject";
+
+export interface Gateway<T> {
+    get(resource:string):Observable<T>;
+};
+export class AxiosGateway<T> implements Gateway<T> {
+    get(resource:string):Observable<T> {
+        const subject = new BehaviorSubject<string>('/');
+        subject.next(resource);
+        return subject
+            .flatMap(resource => Observable.fromPromise(axios.request({
+                baseURL: 'http://localhost:3004/',
+                timeout: 1000,
+                method: 'get',
+                headers: {'X-Custom-Header': 'foobar'},
+                url: resource
+            }))).retry(3)
+            .do((resp) => {
+                console.log("returned from call for resource: ", resource, JSON.stringify(resp));
+            })
+            .map((resp) => resp.data);
+    }
+}
