e73a03eac4d3e7d24d0e6dfa9c345992c0c2216b
christian.ciceri <christian.ciceri@apiumtech.com>
2016-09-15 17:55:48 +0200

    OK with lenses

--- tests/myFirstTest.ts
+++ tests/myFirstTest.ts
@@ -1,16 +1,43 @@
 import {Hero} from '../app/hero';
 import {Subject} from 'rxjs/Rx';
 import {expect} from 'chai';
+import * as R from 'ramda';
+import arrayContaining = jasmine.arrayContaining;
 
 describe("first test", () => {
     it('has name', () => {
         let hero:Hero = {id: 3, name: 'Super Cat'}; //structural type
         expect(hero.name).to.equal('Super Cat');
     });
-    it("can subscribe", function()
-    {
+    it("can subscribe", function () {
         let a = new Subject();
         a.subscribe(()=>console.log("hola 2"));
         a.next({})
     });
+    describe("rambda", () => {
+        describe("lenses", () => {
+            it("should work", (done) => {
+                const aPerson={
+                    'name': 'beppe',
+                    'father': {
+                        'name':'gianluigi',
+                        'age': 65
+                    }
+                }
+                const lens= R.lensPath(['father', 'age']);
+                const aNewPerson = R.set(lens, 66, aPerson);
+                expect(aNewPerson).to.eql({
+                                    'name': 'beppe',
+                                    'father': {
+                                        'name':'gianluigi',
+                                        'age': 66
+                                    }
+                                })
+                done();
+            });
+            ;
+        })
+        ;
+    })
+
 });
