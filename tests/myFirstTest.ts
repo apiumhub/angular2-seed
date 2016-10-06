99e426fc1189112866e9ed733571f148c0ada04a
christian.ciceri <christian.ciceri@apiumtech.com>
2016-10-06 15:09:13 +0200

    changed name

--- tests/myFirstTest.ts
+++ tests/myFirstTest.ts
@@ -9,7 +9,7 @@ import arrayContaining = jasmine.arrayContaining;
 
 describe("first test", () => {
     it('has name', () => {
-        let hero:Hero = {id: 3, name: 'Super Cat'}; //structural type
+        let hero: Hero = {id: 3, name: 'Super Cat'}; //structural type
         expect(hero.name).to.equal('Super Cat');
     });
     it("can subscribe", () => {
@@ -22,7 +22,7 @@ describe("first test", () => {
 
         describe("lenses", () => {
             it("should work", (done) => {
-                const aPerson:any = {
+                const aPerson: any = {
                     'name': 'beppe',
                     'father': {
                         'name': 'gianluigi',
@@ -46,7 +46,7 @@ describe("first test", () => {
         describe("append", () => {
             describe("on deep object with collection", () => {
                 it("should work", function (done) {
-                    const aPerson:any = {
+                    const aPerson: any = {
                         'name': 'beppe',
                         'father': {
                             'name': 'gianluigi',
@@ -76,35 +76,33 @@ describe("first test", () => {
                 ;
             })
             ;
-            describe("map object", ()=>{
-            	describe("in constructor", ()=>{
-            		it("should work", (done) =>{
-            			class StructType
-                        {
-                            name:string;
-                            surname:string;
-                        }
-                        class NonStructType
-                        {
+            describe("map object", ()=> {
+                describe("in constructor", ()=> {
+                    it("should work", (done) => {
+                        class StructType {
                             name: string;
-                            surname:string;
-                            static fromDTO(dto:StructType):NonStructType
-                            {
-                                const instance=new NonStructType();
+                            surname: string;
+                        }
+                        class NonStructType {
+                            name: string;
+                            surname: string;
+
+                            static fromDTO(dto: StructType): NonStructType {
+                                const instance = new NonStructType();
                                 return mapFromDTO(dto, instance);
                             }
-                            toDto():StructType
-                            {
-                                return {name: "tu",surname:"tutu"};
+
+                            toDto(): StructType {
+                                return {name: "tu", surname: "tutu"};
                             }
                         }
                         const obj: StructType = {name: "yo", surname: "yoyo"};
                         const obj2 = NonStructType.fromDTO(obj);
                         expect(obj2.name).to.eql("yo");
                         expect(obj2.surname).to.eql("yoyo");
-            			done();
-            		});
-            	});
+                        done();
+                    });
+                });
             });
         })
 
@@ -114,26 +112,26 @@ describe("first test", () => {
         describe("mixins", () => {
             it("should work", function (done) {
                 class Trait1 {
-                    hello():string {
+                    hello(): string {
                         return "hello"
                     }
                 }
                 class Trait2 {
-                    world():string {
+                    world(): string {
                         return "world"
                     }
                 }
                 class Mixin implements Trait1, Trait2 {
-                    hello():string {
+                    hello(): string {
                         return "";
                     }
 
-                    world():string {
+                    world(): string {
                         return "";
                     }
 
                 }
-                function applyMixins(derivedCtor:any, baseCtors:any[]) {
+                function applyMixins(derivedCtor: any, baseCtors: any[]) {
                     baseCtors.forEach(baseCtor => {
                         Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
                             derivedCtor.prototype[name] = baseCtor.prototype[name];
@@ -151,7 +149,7 @@ describe("first test", () => {
         ;
         describe("map types", () => {
             it("should work", (done) => {
-                const aPerson:any = {
+                const aPerson: any = {
                     'name': 'beppe',
                     'father': {
                         'name': 'gianluigi',
@@ -162,82 +160,84 @@ describe("first test", () => {
                     }
                 }
 
-                const newPerson=changePropertyMap(aPerson, ['directFamily', 'children'], {key: 'roby', value:{name: 'roby'}})
+                const newPerson = changePropertyMap(aPerson, ['directFamily', 'children'], {
+                    key: 'roby',
+                    value: {name: 'roby'}
+                })
                 expect(newPerson).to.eql({
-                                    'name': 'beppe',
-                                    'father': {
-                                        'name': 'gianluigi',
-                                        'age': 65
-                                    },
-                                    'directFamily': {
-                                        'children': {
-                                            'roby': {'name': 'roby'}
-                                        }
-                                    }
-                                })
+                    'name': 'beppe',
+                    'father': {
+                        'name': 'gianluigi',
+                        'age': 65
+                    },
+                    'directFamily': {
+                        'children': {
+                            'roby': {'name': 'roby'}
+                        }
+                    }
+                })
                 done();
             });
             ;
         })
 
     })
-    describe("rx", ()=>{
-    	describe("wired but not called", ()=>{
-    		it("should wire correctly", (done) =>{
+    describe("rx", ()=> {
+        describe("wired but not called", ()=> {
+            it("should wire correctly", (done) => {
 
-                const subject=new Subject<any>();
-                const obs=subject
+                const subject = new Subject<any>();
+                const obs = subject
                     .flatMap(
-                        (value:any)=>Observable.fromPromise(axios.request({
+                        (value: any)=>Observable.fromPromise(axios.request({
                             baseURL: 'http://localhost:3004/',
                             timeout: 1000,
                             method: 'get',
                             url: 'test-resource'
                         })))
-                obs.do((response:any)=>{
+                obs.do((response: any)=> {
                     console.log(response.data);
                     expect(response.data).to.eql([{}])
                 }).subscribe(()=>done());
                 subject.next("");
-    		});
-    	});
-        describe("BehaviorSubject", ()=>{
-        	describe("has initial value", ()=>{
-        		describe("and flatMap called", ()=>{
-        			it("replays initial value", function(done)
-        			{
+            });
+        });
+        describe("BehaviorSubject", ()=> {
+            describe("has initial value", ()=> {
+                describe("and flatMap called", ()=> {
+                    it("replays initial value", function (done) {
                         const subject = new BehaviorSubject("A");
-                        subject.first().subscribe((value)=>{
+                        subject.first().subscribe((value)=> {
                             expect(value).to.eql("A");
                             done();
                         });
                         subject.next("B");
-        			});
-        		});
-        	});
+                    });
+                });
+            });
         });
-        describe("Subject", ()=>{
-        	describe("has initial value", ()=>{
-        		it("should NOT replay initial value", (done) =>{
+        describe("Subject", ()=> {
+            describe("has initial value", ()=> {
+                it("should NOT replay initial value", (done) => {
                     const subject = new Subject<string>();
-                    subject.first().subscribe((value:string)=>{
+                    subject.first().subscribe((value: string)=> {
                         expect(value).to.not.eql("A");
                         done();
                     });
                     subject.next("B");
-        		});
-        	});
+                });
+            });
         });
-        describe("join", ()=>{
-        	describe("many subjects", ()=>{
-        		it("join them all", (done)=>{
-                    const subj1=new Subject<string>();
-                    const subj2=new Subject<string>();
-                    const subj3=new Subject<string>();
+        describe("join", ()=> {
+            describe("many subjects", ()=> {
+                it("join them all", (done)=> {
+                    const subj1 = new Subject<string>();
+                    const subj2 = new Subject<string>();
+                    const subj3 = new Subject<string>();
                     const subjects: Observable<string>[] = [subj1, subj2, subj3];
-                    const finalObservable:Observable<string>=subjects.reduce((prev: Observable<string>, curr: Observable<string>)=>prev.merge(curr));
+                    const finalObservable: Observable<string> = subjects.reduce((prev: Observable<string>, curr: Observable<string>)=>prev.merge(curr));
                     let values: string[] = [];
-                    finalObservable.subscribe((value:string)=>{
+                    finalObservable.subscribe((value: string)=> {
                         console.log(value);
                         values.push(value);
                     });
@@ -246,21 +246,21 @@ describe("first test", () => {
                     subj3.next("C");
                     expect(values.join()).to.eql("A,B,C");
                     done();
-        		})
-        	})
+                })
+            })
         });
-        describe("hot observable", ()=>{
-        	it("should run map even if not subscribed (are lazy)", (done) =>{
-        		const subj=new Subject<string>();
-        		const obs=subj.do((value:string)=>{
+        describe("hot observable", ()=> {
+            it("should run map even if not subscribed (are lazy)", (done) => {
+                const subj = new Subject<string>();
+                const hotObs = subj.do((value: string)=> {
                     console.log(value);
                     expect(value).to.eql("hola");
                     done();
                 }).publish(); //creates a new ConnectableObservable
-                obs.refCount();
-                obs.connect(); //internally a subscribe
+                hotObs.refCount();
+                hotObs.connect(); //internally a subscribe
                 subj.next("hola");
-        	});
+            });
         });
     });
 
